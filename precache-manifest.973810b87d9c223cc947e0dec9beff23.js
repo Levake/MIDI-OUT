2fcb83167e435906f52ff284d2c4de27a59ec13a
Levake <stars.programm@gmail.com>
2020-08-06 09:29:21 +0700

    Updates

---
+++ precache-manifest.973810b87d9c223cc947e0dec9beff23.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ad579903f12a1bf33",
+    "url": "/MIDI-OUT/static/css/main.11cdae30.chunk.css"
+  },
+  {
+    "revision": "4e1ad579903f12a1bf33",
+    "url": "/MIDI-OUT/static/js/main.c0412a85.chunk.js"
+  },
+  {
+    "revision": "65eb99d5265f4e0c5a22",
+    "url": "/MIDI-OUT/static/js/runtime~main.256065dd.js"
+  },
+  {
+    "revision": "7d04b3a10ad50e1e70bf",
+    "url": "/MIDI-OUT/static/css/2.d6a0edde.chunk.css"
+  },
+  {
+    "revision": "7d04b3a10ad50e1e70bf",
+    "url": "/MIDI-OUT/static/js/2.75a32d6f.chunk.js"
+  },
+  {
+    "revision": "25ab035e8e031eebad27c77c9fe14827",
+    "url": "/MIDI-OUT/static/media/schasliviibilet.25ab035e.png"
+  },
+  {
+    "revision": "e7e8a02d000582f7d1f0e350be460a0c",
+    "url": "/MIDI-OUT/static/media/kosNoN.e7e8a02d.png"
+  },
+  {
+    "revision": "81320fcd7f7e0bdb8909d7e43b340133",
+    "url": "/MIDI-OUT/static/media/kos1.81320fcd.png"
+  },
+  {
+    "revision": "2c3f5fdd6ecddbdbdc0d15a4b1e520ee",
+    "url": "/MIDI-OUT/static/media/kos2.2c3f5fdd.png"
+  },
+  {
+    "revision": "8313fcaa84224006783d40c211349ef3",
+    "url": "/MIDI-OUT/static/media/kos3.8313fcaa.png"
+  },
+  {
+    "revision": "e6ba8effa1e17dd15abdb50d458efa00",
+    "url": "/MIDI-OUT/static/media/kos4.e6ba8eff.png"
+  },
+  {
+    "revision": "a5b712930a7bb4681103df92d479d10e",
+    "url": "/MIDI-OUT/static/media/kos5.a5b71293.png"
+  },
+  {
+    "revision": "a3766195becd1e6815af8f797e0448f0",
+    "url": "/MIDI-OUT/static/media/kos6.a3766195.png"
+  },
+  {
+    "revision": "197537e063266dd28bba1c2b743a0eac",
+    "url": "/MIDI-OUT/static/media/orel.197537e0.png"
+  },
+  {
+    "revision": "271608c7fbabccad6d690863fb9b6cae",
+    "url": "/MIDI-OUT/static/media/reshka.271608c7.png"
+  },
+  {
+    "revision": "cf9f3c67d7060855b764d51f2c310d52",
+    "url": "/MIDI-OUT/static/media/orelandreshka.cf9f3c67.png"
+  },
+  {
+    "revision": "ac7dc58d6dd7f179b30857a93367147e",
+    "url": "/MIDI-OUT/index.html"
+  }
+];
